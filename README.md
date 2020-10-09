9c62f791c098ffb320024bdc94f78cfc04e577d3
santorsm <matt.santorsola@gmail.com>
2020-10-08 20:06:50 -0700

    updating lab to match work

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-# 201-lab-03 
+# 201-lab-02 - 03
 
 ## About Me
 
- - This is a webpage "about me" it will:
+ - This is a webpage "about me". It will:
    - Give you some basic information about who I am and where I come from.
    - Quiz you on these fun facts
-
 
 ## Features
 
 <br/>
 
-### To Do:
+### **To Do:**
 
-prompt user to guess a number  <br/>  [x] prompt added <br/>
-<br/>display all possible correct answers at the end <br/>  [x] display `rapini`, `manicotti`, and `calamari` at the end regardless of outcome<br/>
+**prompt user to guess a number**  <br/>  [x] prompt added <br/>
+<br/>**display all possible correct answers at the end** <br/>  [x] display `rapini`, `manicotti`, and `calamari` at the end regardless of outcome<br/>
 
-tally the total number of correct answers <br/>
+**tally the total number of correct answers** <br/>
 [x] display total number of correct answers
 
 <br/>
--- js/app.js
+++ js/app.js
@@ -5,53 +5,54 @@ var userScore = 0;
 var userName = prompt('Welcome to my website; what\'s your name?');
 console.log('User\'s name is: ' + userName);
 
-// alert('Hello ' + userName + '; nice to meet you!');
+alert('Hello ' + userName + '; nice to meet you!');
 
-// // Matt - Question 1:
-// var userReady = prompt('Are you ready for some questions about me?').toLowerCase();
+// Matt - Question 1:
+var userReady = prompt('Are you ready for some questions about me?').toLowerCase();
 
-// if (userReady === 'yes' || userReady === 'y') {
-//   // console.log('Great!  Let\'s go!');
-//   userScore++;
-//   alert('Great!  Let\'s go!');
-// }
+if (userReady === 'yes' || userReady === 'y') {
+  // console.log('Great!  Let\'s go!');
+  userScore++;
+  alert('Great!  Let\'s go!');
+}
 
-// // Matt - Question 2:
-// var responseSibling = prompt('Do I have any sisters?').toLowerCase();
+// Matt - Question 2:
+var responseSibling = prompt('Do I have any sisters?').toLowerCase();
 
-// if (responseSibling === 'no' || responseSibling === 'n') {
-//   // console.log('Correct! I do\'nt have anu sisters');
-//   userScore++;
-//   alert('Correct! I don\'t have any sisters');
-// }
+if (responseSibling === 'no' || responseSibling === 'n') {
+  // console.log('Correct! I do\'nt have anu sisters');
+  userScore++;
+  alert('Correct! I don\'t have any sisters');
+}
 
-// // Matt - Question 3:
-// var responseHome = prompt('Am I originally from Seattle?').toLowerCase();
+// Matt - Question 3:
+var responseHome = prompt('Am I originally from Seattle?').toLowerCase();
 
-// if (responseHome === 'no' || responseHome === 'n') {
-//   // console.log('Correct! I\'m originally from New York');
-//   userScore++;
-//   alert('Correct! I\'m originally from New York');
-// }
+if (responseHome === 'no' || responseHome === 'n') {
+  // console.log('Correct! I\'m originally from New York');
+  userScore++;
+  alert('Correct! I\'m originally from New York');
+}
 
-// // Matt - Question 4:
-// var responseUsaf = prompt('Did I serve in the USAF?').toLowerCase();
+// Matt - Question 4:
+var responseUsaf = prompt('Did I serve in the USAF?').toLowerCase();
 
-// if (responseUsaf === 'yes' || responseUsaf === 'y') {
-//   // console.log('Correct! I was a pilot in the USAF');
-//   userScore++;
-//   alert('Correct! I was a pilot in the USAF');
-// }
+if (responseUsaf === 'yes' || responseUsaf === 'y') {
+  // console.log('Correct! I was a pilot in the USAF');
+  userScore++;
+  alert('Correct! I was a pilot in the USAF');
+}
 
-// // Matt - Question 5:
-// var responseDogs = prompt('Do I have any pets?').toLowerCase();
+// Matt - Question 5:
+var responseDogs = prompt('Do I have any pets?').toLowerCase();
 
-// if (responseDogs === 'yes' || responseDogs === 'y') {
-//   // console.log('Correct! I have 2 dogs a golden retriever and german shepard');
-//   userScore++;
-//   alert('Correct! I have 2 dogs a golden retriever and german shepard');
-// }
+if (responseDogs === 'yes' || responseDogs === 'y') {
+  // console.log('Correct! I have 2 dogs a golden retriever and german shepard');
+  userScore++;
+  alert('Correct! I have 2 dogs a golden retriever and german shepard');
+}
 
+// Matt - Question 6:
 alert(`Hey ${userName}, I hear you like guessing numbers. I'll provide feedback to help you zero-in on the number.
 
 Your inputs will establish the lower and upper bound of the range of possible numbers. Please enter only integers.
@@ -100,7 +101,7 @@ if (i + 1 > size){
   Thanks for playing!`);
 }
 
-
+// Matt - Question 7:
 
 var maxAttempts = 6;
 var answeredCorrectly = false;
